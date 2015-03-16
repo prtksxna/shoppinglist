3aadc0dec08eef6528263e55726e74929f382fd2
Mun May Tee <munmay.g@gmail.com>
2015-03-16 00:47:14 -0700

    lotsa stuff

--- js/main.js
+++ js/main.js
@@ -1,8 +1,110 @@
+// <INPUT> TEMPLATE //
+
+var anotherListItem = '<div class="form-list"><button type="button" class="delete-list"></button><button type="button" class="undone-list"></button><button type="button" class="done"></button><input type="text"></div>';
+
+
 $(document).ready(function() {
-    $('h2.done-count').hide()
-    
-    $('.form-list').on('keydown','input',function(k){            
+
+    $('input:first').focus();
+
+    // ADDING MORE ITEMS ON THE LIST //
+
+    //  $(originOfInteraction).on('event', filteroriginOfInteraction, function(parameter) {
+
+    $(document).on('keydown', 'input', function(dataFromKeyDownEvent) {
+        // When enter is pushed
+        if (dataFromKeyDownEvent.keyCode == 13) {
+            dataFromKeyDownEvent.preventDefault();
+
+            // Show checkmark only when enter is pushed
+            $(this).prev().fadeIn(500);
+
+            // If this input is the last input
+            if ($(this).is('input:last')) {
+
+                // Create another line after the last <input>
+                $('form').append(anotherListItem);
+            }
+
+            // Go back up to parent, go to the next parent element, and within the element, find <input>, and give it focus
+            $(this).parent().next().find('input').focus();
         }
     });
-});
 
+    
+    // PRESSING TAB FOCUSES THE NEXT LINE
+    $(document).on('keydown', 'input', function(tab){
+        if (tab.keyCode == 9) {
+            tab.preventDefault();
+            $(this).parent().next().find('input').focus();
+            }
+    });
+    
+
+    // STRIKETHROUGH ON HOVER
+//    $(document).on('mouseover', '.done', function() {
+//        $(this).next().css("text-decoration", "line-through");
+//    });
+
+//    // UN-STRIKETHROUGH ON MOUSEOUT
+//    $(document).on('mouseout', '.done', function() {
+//        $(this).next().css("text-decoration", "none");
+//    });
+    
+    // STRIKETHROUGH WHEN DONE IS CLICKED
+    $(document).on('click', '.done', function() { // DONE IS CLICKED
+        $(this).parent().find('input').css("text-decoration", "line-through"); //STRIKETHROUGH DONE ITEM
+        $(this).parent().find('.done').hide(); //HIDE DONE BUTTON 
+        $(this).parent().find('.undone-list').show(); //SHOW UN-DONE BUTTON
+    });
+    
+    
+//    // SHOW OPTION TO UN-DONE ON HOVER STRIKETHROUGH
+//    $(document).on('mouseover', 'input', function(doneHidden) {
+//        // CHECK IF AN ITEM IS STRIKETHROUGH. ONLY WHEN IT IS,
+//        if (doneHidden.
+//            
+//        // SHOW OPTION TO UN-DONE ON HOVER
+//        $(this).parent().find('.undone-list').show();
+//    });
+//    
+//    // MOUSEOU TO REMOVE UN-DONE OPTION
+//    $(document).on('mouseout', 'input', function() {
+//        $(this).parent().find('.undone-list').hide();
+//    });
+    
+    
+    // UN-DONE-ING AN ITEM
+    $(document).on('click', 'button.undone-list', function() { //UN-DONE BUTTON IS CLICKED
+        $(this).parent().find('input').css("text-decoration", "none"); //UNSTRIKETHROUGH ITEM
+        $(this).parent().find('.undone-list').hide(); //HIDE UN-DONE BUTTON
+        $(this).parent().find('.done').show(); //SHOW DONE BUTTON
+    });
+    
+
+// DELETING ITEM(S) //
+
+    $(document).on('click', '.delete', function() { // When eraser is clicked
+        $('.done').fadeOut(400); // "Done" buttons disappear
+        $('.delete').animate({right: "35em"}, 300);// ERASER DISAPPEAR
+        $('.undone-list').fadeOut(400); // "Undone" buttons disappear
+            $('button.delete-list').fadeIn(500); // "Delete buttons appear
+        });
+    
+    $(document).on('click', '.delete-list', function() { // WHEN DELETE BUTTON IS CLICKED
+        $('.done','.undone-list').hide();// ALL DONE AND UNDONE BUTTONS HIDE
+        $(this).parent().children().fadeOut(230, function() { // "Delete-list" button and that particular <div></div> will fade out and disappear
+            $(this).parent().slideUp(800, function() { // Animation to slide remaining <div>'s up
+                $(this).remove(); //Once <div></div> has disappeared
+            });
+        });
+    });
+    
+    
+// DONE DELETING ITEM //
+
+    
+    
+    
+    
+});
